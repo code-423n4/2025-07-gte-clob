79fbdd53b62053b6b37a970f4097eaf940a70d77
Alexander Papageorgiou <alex.ppg@protonmail.com>
2025-07-25 07:08:48 -0700

    Code: Inclusion of Executable PoC

---
+++ test/c4-poc/PoC.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {PoCTestBase} from "./PoCTestBase.t.sol";
+
+contract PoC is PoCTestBase {
+    function test_submissionValidity() external {
+    }
+}
